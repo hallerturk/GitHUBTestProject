3d244484cb08a63bef48df4b6c8a5a9e4fc75f64
hallerturk <halilerturk91@gmail.com>
2023-11-11 19:57:51 -0500

    v2

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -2,5 +2,8 @@ public class Class1 {
     public static void main(String[] args) {
         //version 1
         System.out.println("version 1");
+
+        //version 2
+        System.out.println("version 2");
     }
 }
